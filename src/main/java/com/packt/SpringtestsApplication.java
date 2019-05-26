ca595d44d9284d303177349c05fe92e8a2ba5160
Tadeusz Wolski <tadeusz.wolski@capgemini.com>
2019-05-26 15:12:54 +0200

    Initial commit

---
+++ src/main/java/com/packt/SpringtestsApplication.java
@@ -0,0 +1,13 @@
+package com.packt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringtestsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringtestsApplication.class, args);
+    }
+
+}
